cb4a4b9b45f0ba5c02a72da640d4bc0451164420
10129255 <10129255@A23141889.zte.intra>
2015-09-17 17:31:03 +0800

    Make the last view expend the rest space when the screen is not full

--- MeWidget/src/com/example/mewidget/view/SpreadListView.java
+++ MeWidget/src/com/example/mewidget/view/SpreadListView.java
@@ -190,15 +190,22 @@ public class SpreadListView extends AdapterView<CursorAdapter> {
 			params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 		}
 		addViewInLayout(child, viewPos, params, true);
-		
-        if(position == openPosition){
+
+        if(mAdapter.getCount() > 0 && position == openPosition){
 			openView = child;
 			child.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
 					MeasureSpec.makeMeasureSpec(((int)getHeight()/6)*3, MeasureSpec.EXACTLY));
 		}
 		else{
-			child.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
-					MeasureSpec.makeMeasureSpec((int)getHeight()/6, MeasureSpec.EXACTLY));
+			if(child == lastView && position < 3){
+				//when screen is not full, then spread the last view height
+				child.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec((int)getHeight()/(position+1), MeasureSpec.EXACTLY));
+			}
+			else{
+				child.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+						MeasureSpec.makeMeasureSpec((int)getHeight()/6, MeasureSpec.EXACTLY));
+			}
 		}
 	}
  
